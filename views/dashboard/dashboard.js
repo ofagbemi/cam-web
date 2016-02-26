3df648e2d1f5caf111add585ed3d4d8fd58e80c8
Jare Fagbemi <jare@rescomp-15-307854.stanford.edu>
2016-02-26 12:11:23 -0800

    Show empty tag for active goals

--- views/dashboard/dashboard.js
+++ views/dashboard/dashboard.js
@@ -17,12 +17,26 @@ Dashboard.prototype.init = function() {
     this.reviewGoalComponents.push(goal);
   }, this));
 
-  // set up goals still
+  // set up active goals
+  this.$activeGoalsContainer = this.$el.find('#active-goals');
+  this.$activeGoals = this.$activeGoalsContainer.find('[data-component="goal-card"]');
+  this.activeGoalComponents = [];
+  this.$activeGoals.each(_.bind(function(i, el) {
+    var goal = ComponentFactory.getComponent(el);
+    goal.on('hide', _.bind(this.onActiveGoalHide, this));
+    this.activeGoalComponents.push(goal);
+  }, this));
 };
 
 Dashboard.prototype.onReviewGoalHide = function() {
-  if (!this.$reviewGoalsContainer.find('[data-component="goal"]:not(.hide)').length) {
+  if (!this.$reviewGoalsContainer.find('[data-component="goal-card"]:not(.hide)').length) {
     this.$reviewGoalsContainer.addClass('empty');
+  }
+};
+
+Dashboard.prototype.onActiveGoalHide = function() {
+  if (!this.$activeGoalsContainer.find('[data-component="goal-card"]:not(.hide)').length) {
+    this.$activeGoalsContainer.addClass('empty');
   }
 };
 
