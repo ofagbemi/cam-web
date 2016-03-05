1885a9ea0ce7edb049147b6e1b5198996b2fd9fa
Jare Fagbemi <ofagbemi@stanford.edu>
2016-03-05 12:21:36 -0800

    Start working on add milestones component

--- components/create-mission-card/create-mission-card.js
+++ components/create-mission-card/create-mission-card.js
@@ -10,7 +10,7 @@ function CreateMissionCard($el) {
 CreateMissionCard.prototype.init = function() {
   this.titleComponent = ComponentFactory.getComponent(this.$el.find('[data-component="text-box"].title'));
 
-  $('html').on('click', _.bind(this._handleHtmlClick, this));
+//  $('html').on('click', _.bind(this._handleHtmlClick, this));
   this.$el.on('click', _.bind(this._handleClick, this));
 };
 
@@ -21,14 +21,14 @@ CreateMissionCard.prototype._handleClick = function() {
   this.titleComponent.focus();
 };
 
-CreateMissionCard.prototype._handleHtmlClick = function(e) {
-  var root = this.$el.get(0);
-  if (root !== e.target && !$.contains(root, e.target)) {
-    this.blur();
-  }
-};
+//CreateMissionCard.prototype._handleHtmlClick = function(e) {
+//  var root = this.$el.get(0);
+//  if (root !== e.target && !$.contains(root, e.target)) {
+//    this.blur();
+//  }
+//};
 
-CreateMissionCard.prototype.blur = function() {
+CreateMissionCard.prototype.close = function() {
   this.$el.removeClass('active');
 };
 
