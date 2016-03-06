d0c08ca5848a53ef5d5e68b20d844b6e6aa394b7
Jare Fagbemi <ofagbemi@stanford.edu>
2016-03-06 02:39:09 -0800

    Add splash route
    Redirects to dashboard

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -3,5 +3,5 @@ var router = require('express').Router();
 var dummyData = require('../../server/dummy/dashboard.json');
 
 module.exports = router.get('/', function(req, res) {
-    res.render('dashboard/dashboard', dummyData);
+  res.render('dashboard/dashboard', dummyData);
 });
